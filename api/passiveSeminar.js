262815245180319d2e095a08ae8b30f7ef93f2b7
ludwik.applechina@gmail.com <haosun@hcdglobal.com>
2013-11-13 13:04:18 +0800

    server improve

--- api/passiveSeminar.js
+++ api/passiveSeminar.js
@@ -11,19 +11,37 @@ exports.passiveSeminar = function(io){
 		period : req.body.period, 
 		cgiHost : conf.cgi.host, 
 		cgiPort : conf.cgi.port,
-		cgiPath : conf.cgi.path_producerDecision
+		cgiPath : conf.cgi.path_producerDecision,
+		startFrom: -3, 
+		endWith: 0
 	}	
-		
+			
 	//export to binary, negotiations, P1/P2/P3, R1/R2
 	require('./models/producerDecision.js').exportToBinary(options).then(function(result){
-            io.sockets.emit('AdminProcessLog', { msg: result.msg, isError: false });			
-			res.send(200, 'complete');
+        io.sockets.emit('AdminProcessLog', { msg: result.msg, isError: false });			
+		options.producerID = '2';
+		return require('./models/producerDecision.js').exportToBinary(options);
+	}).then(function(result){
+        io.sockets.emit('AdminProcessLog', { msg: result.msg, isError: false });			
+		options.producerID = '3';
+		return require('./models/producerDecision.js').exportToBinary(options);
+	}).then(function(result){
+        io.sockets.emit('AdminProcessLog', { msg: result.msg, isError: false });			
+		options.retailerID = '1';
+		options.cgiPath = conf.cgi.path_retailerDecision;
+		return require('./models/retailerDecision.js').exportToBinary(options);
+	}).then(function(result){
+        io.sockets.emit('AdminProcessLog', { msg: result.msg, isError: false });			
+		options.retailerID = '2';
+		return require('./models/retailerDecision.js').exportToBinary(options);
+	}).then(function(result){
+        io.sockets.emit('AdminProcessLog', { msg: result.msg, isError: false });			
+		res.send(200, 'complete');	
 	}, function(error){ //log the error
-			console.log(error.msg)
-            io.sockets.emit('AdminProcessLog', { msg: error.msg, isError: true });			
-            res.send(300, 'error');            
+        io.sockets.emit('AdminProcessLog', { msg: error.msg, isError: true });			
+        res.send(300, 'error');            
 	}, function(progress){ //log the progress
-            io.sockets.emit('AdminProcessLog', { msg: progress.msg, isError: false });			
+        io.sockets.emit('AdminProcessLog', { msg: progress.msg, isError: false });			
 	})
 	
 	//call passive module on the server, callback...
