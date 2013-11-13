81a33be9fc11e9540cff67fcfd8f25b098f00b8c
monsoul <monsoul@msn.com>
2013-11-13 15:14:07 +0800

    admin login

--- app/js/controllers/loginCtrl.js
+++ app/js/controllers/loginCtrl.js
@@ -1,6 +1,6 @@
 define(['app','socketIO'], function(app) {
 
-	app.controller('loginCtrl',['$scope', '$http', 'ProducerDecisionBase','$rootScope', function($scope, $http, ProducerDecisionBase,$rootScope) {
+	app.controller('loginCtrl',['$scope', '$http', '$location','$rootScope', function($scope, $http, $location,$rootScope) {
 		// You can access the scope of the controller from here
 		$scope.welcomeMessage = 'hey this is loginCtrl.js!';
 		$scope.loginOpts = {
@@ -14,16 +14,33 @@ define(['app','socketIO'], function(app) {
 		$scope.openLoginModal=function(){
 			$scope.loginModal=true;
 		}
-		$scope.closeLoginModal=function(){
+		var closeLoginModal=function(){
 			$scope.loginModal=false;
 		}
 
 		$scope.openAdminLoginModal=function(){
 			$scope.aminLoginModal=true;
 		}
-		$scope.closeAdminLoginModal=function(){
+		var closeAdminLoginModal=function(){
 			$scope.aminLoginModal=false;
+			$scope.bubleClassName = "";    
+            $scope.bubleTitle = "";
+            $scope.bubleMsg = "";
 		}
+
+		$scope.adminLogin=function(){
+			if($scope.adminSeminar=="MAY"&&$scope.adminPassword=="123"){
+				closeAdminLoginModal();
+				$location.path('/admin');
+			}else{
+				$scope.bubleClassName = "alert alert-danger";    
+                $scope.bubleTitle = "Error";
+                $scope.bubleMsg = " Failed to login";
+			}
+		}
+
+		$scope.closeLoginModal=closeLoginModal;
+		$scope.closeAdminLoginModal=closeAdminLoginModal;
 
 	}]);
 
