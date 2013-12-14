8d5908197eda158a1596d8b8210956d750a2e278
monsoul <monsoul@msn.com>
2013-12-14 19:08:15 +0800

    update

--- app/js/controllers/producerDecisionStep1ctrl.js
+++ app/js/controllers/producerDecisionStep1ctrl.js
@@ -60,6 +60,8 @@ define(['app'], function(app) {
 					$scope.checkDesign=checkDesign;
 					$scope.checkTechnology=checkTechnology;
 					$scope.checkRMQ=checkRMQ;
+					$scope.checkCurrentBM=checkCurrentBM;
+					$scope.checkCurrentEMall=checkCurrentEMall;
 
 					$scope.addNewProduct=addNewProduct;
 					$scope.updateProducerDecision=updateProducerDecision;
@@ -519,22 +521,78 @@ define(['app'], function(app) {
 			}
 
 			var checkCurrentBM=function(category,brandName,varName,location,additionalIdx,index,value){
-				var d=$q.defer;
-				var categoryID=0,max=0;
+				var d=$q.defer();
+				var categoryID=0,max=0,currentUnitCost=0;
 				var filter=/^[0-9]+([.]{1}[0-9]{1,2})?$/;
 				if(!filter.test(value)){
 					d.resolve('Input a number');
 				}
+				if(category=="Elecssories"){
+					categoryID=1;
+				}else{
+					categoryID=2;
+				}
+				var postData = {
+					period : $rootScope.currentPeriod,
+					seminar : $rootScope.user.seminar,
+					brandName : brandName,
+					varName : varName,
+					catID : categoryID,
+					userRole :  $rootScope.userRoles.producer,
+					userID : $rootScope.user.roleID,								
+				}
+				$http({
+					method:'POST',
+					url:'/getCurrentUnitCost',
+					data:postData
+				}).then(function(data){
+					currentUnitCost=data.data.result;
+					if(value>4*currentUnitCost||value<0.5*currentUnitCost){
+						d.resolve('Input range:'+0.5*currentUnitCost+'~'+4*currentUnitCost);
+					}else{
+						d.resolve();
+					}
+				},function(){
+					d.resolve('fail');
+				})
 				return d.promise;
 			}
 
 			var checkCurrentEMall=function(category,brandName,varName,location,additionalIdx,index,value){
-				var d=$q.defer;
-				var categoryID=0,max=0;
+				var d=$q.defer();
+				var categoryID=0,max=0,currentUnitCost=0;
 				var filter=/^[0-9]+([.]{1}[0-9]{1,2})?$/;
 				if(!filter.test(value)){
 					d.resolve('Input a number');
 				}
+				if(category=="Elecssories"){
+					categoryID=1;
+				}else{
+					categoryID=2;
+				}
+				var postData = {
+					period : $rootScope.currentPeriod,
+					seminar : $rootScope.user.seminar,
+					brandName : brandName,
+					varName : varName,
+					catID : categoryID,
+					userRole :  $rootScope.userRoles.producer,
+					userID : $rootScope.user.roleID,								
+				}
+				$http({
+					method:'POST',
+					url:'/getCurrentUnitCost',
+					data:postData
+				}).then(function(data){
+					currentUnitCost=data.data.result;
+					if(value>6*currentUnitCost||value<0.5*currentUnitCost){
+						d.resolve('Input range:'+0.5*currentUnitCost+'~'+6*currentUnitCost);
+					}else{
+						d.resolve();
+					}
+				},function(){
+					d.resolve('fail');
+				})
 				return d.promise;
 			}
 
