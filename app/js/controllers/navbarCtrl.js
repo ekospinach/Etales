cab5945643aac823cff92a349e02574111103ae1
monsoul <monsoul@msn.com>
2013-12-15 15:26:43 +0800

    update

--- app/js/controllers/navbarCtrl.js
+++ app/js/controllers/navbarCtrl.js
@@ -5,7 +5,14 @@ define(['app'], function(app) {
 
 	//        console.log('trying to get user role text:' + _.invert(Auth.userRoles)[role]);
 	        return _.invert(Auth.userRoles)[role].toUpperCase() + ' ' + $rootScope.user.roleID;
-	    };		
+	    };
+
+	    $scope.getRoleNormal=function(role){
+	    	var normal=_.invert(Auth.userRoles)[role];
+	    	normal=normal.substring(0,1).toUpperCase();
+	    	return _.invert(Auth.userRoles)[role].substring(0,1).toUpperCase()+_.invert(Auth.userRoles)[role].substring(1,_.invert(Auth.userRoles)[role].length);
+	    }
+
 	    $scope.logout = function(){
 	        console.log('trying to logout...');
 	        Auth.logout(function() {
