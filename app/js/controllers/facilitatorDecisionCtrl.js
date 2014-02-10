ead3ac32340de9d533e753bf4354105310d4f050
monsoul <monsoul@msn.com>
2014-02-10 14:41:55 +0800

    update

--- app/js/controllers/facilitatorDecisionCtrl.js
+++ app/js/controllers/facilitatorDecisionCtrl.js
@@ -1,13 +1,16 @@
 define(['app'], function(app) {
 		app.controller('facilitatorDecisionCtrl',
-			['$scope','$q','$rootScope','$location','$http','$filter','Label', function($scope,$q,$rootScope,$location,$http,$filter,Label) {
+			['$scope','$q','$rootScope','$location','$http','$filter','Label','PlayerInfo', function($scope,$q,$rootScope,$location,$http,$filter,Label,PlayerInfo) {
 			$rootScope.decisionActive="active";
 			$rootScope.loginCss="";
 		    $rootScope.loginFooter="bs-footer";
 		    $rootScope.loginLink="footer-links";
 		    $rootScope.loginDiv="container";
-
 		    $scope.userRole=1;
+		    PlayerInfo.setPlayer(1);
 
+		    $scope.setPlayer=function(userRole){
+		    	PlayerInfo.setPlayer(userRole);
+		    }
 		}]);
 });
