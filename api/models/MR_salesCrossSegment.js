fd3271d753270843ec9f5824bb4da6dd4a1d6e01
monsoul <monsoul@msn.com>
2014-04-21 23:08:52 +0800

    update

--- api/models/MR_salesCrossSegment.js
+++ api/models/MR_salesCrossSegment.js
@@ -5,53 +5,6 @@ var mongoose = require('mongoose'),
     request = require('request'),
     q = require('q');
 
-// var MR_salesCrossSegmentSchema = mongoose.Schema({
-//     period : Number,
-//     seminar : String,
-//     absoluteValue     : [variantInfoSchema],
-//     valueChange       : [variantInfoSchema],
-//     absoluteVolume    : [variantInfoSchema],
-//     volumeChange      : [variantInfoSchema],
-// })
-
-// var variantInfoSchema = mongoose.Schema({
-//     variantName       : String,
-//     parentBrandName   : String,
-//     parentCategoryID  : Number,
-//     parentCompanyID   : Number,
-//     marketInfo: [marketInfoSchema]
-// })
-
-// var marketInfoSchema = mongoose.Schema({
-//     marketID : Number, //TMarkets : 1~2
-//     segmentInfo : [segmentInfoSchema]
-// })
-
-// var segmentInfoSchema = mongoose.Schema({
-//     segmentID : Number, //TSegmentsTotal : 1~(4+1)x,
-//     /*
-//         Elecssories:
-//         1-PriceSensitive
-//         2-Value for Money
-//         3-Fashion
-//         4-Freaks
-//         5-Total
-
-//         HealthBeauties:
-//         1-PriceSensitive
-//         2-Value for Money
-//         3-Health Conscious
-//         4-Impatient      
-//         5-Total
-//     */
-//     shopperInfo : [shopperInfoSchema]
-// })
-
-// var shopperInfoSchema = mongoose.Schema({
-//     shoperKind : String, // BMS, NETIZENS, MIXED, ALLSHOPPERS
-//     value : Number,
-// })
-
 //New Schema
 var MR_salesCrossSegmentSchema = mongoose.Schema({
     period : Number,
