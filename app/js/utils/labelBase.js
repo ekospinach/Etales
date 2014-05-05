a4e028eb0aed20174e03477751ca47a64852ad01
monsoul <monsoul@msn.com>
2014-05-05 10:40:45 +0800

    update

--- app/js/utils/labelBase.js
+++ app/js/utils/labelBase.js
@@ -838,7 +838,7 @@ var getLabelBase = function(){
 		{id:'mins',  ENG:'mins',  CHN:'分钟',  RUS:''},
 		{id:'Decision Info',  ENG:'Decision Info',  CHN:'决策介绍',  RUS:''},
 		{id:'Shelf Space Left',  ENG:'Shelf Space Left',  CHN:'货架剩余空间',  RUS:''},
-		{id:'',  ENG:'',  CHN:'',  RUS:''},
+		{id:'Store Management Info',  ENG:'Store Management Info',  CHN:'Store Management 介绍',  RUS:''},
 		{id:'',  ENG:'',  CHN:'',  RUS:''},
 		{id:'',  ENG:'',  CHN:'',  RUS:''},
 		{id:'',  ENG:'',  CHN:'',  RUS:''},
