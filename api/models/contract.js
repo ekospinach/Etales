1c51faab4dee850aaae0069dd55d89651f56a28b
ludwik.applechina@gmail.com <ludwik.applechina@gmail.com>
2014-05-05 16:19:24 +0800

    update negotiation

--- api/models/contract.js
+++ api/models/contract.js
@@ -26,6 +26,33 @@ var contractSchema = mongoose.Schema({
 
 contractSchema.plugin(uniqueValidator);
 
+var contractVariantDetailsSchema = mongoose.Schema({
+	contractCode    : String,
+	parentBrandName : String,
+	parentBrandCount : Number,
+	variantName     : String,
+	variantCount    : Number,
+	
+	nc_MinimumOrder : Number,
+	nc_MinimumOrder_lastModifiedBy : String, 
+	nc_VolumeDiscountRate : Number,
+	nc_VolumeDiscountRate_lastModifiedBy : String,	
+
+	nc_SalesTargetVolume : Number,
+	nc_SalesTargetVolume_lastModifiedBy : String,
+	nc_PerformanceBonusRate : Number,
+	nc_PerformanceBonusRate_lastModifiedBy : String,
+
+	nc_PaymentDays : Number,
+	nc_PaymentDays_lastModifiedBy : String,
+	nc_OtherCompensation : Number,
+	nc_OtherCompensation_lastModifiedBy : String,
+	
+	isProducerApproved : Boolean,
+	isRetailerApproved : Boolean,
+})
+
+
 var contractDetailsSchema = mongoose.Schema({
 	contractCode : String, 
 	userType : String, // 'P' or 'R'
