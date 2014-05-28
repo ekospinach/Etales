7ff6f6838850123210c84fc635b09c3688385e7e
monsoul <monsoul@msn.com>
2014-05-28 18:57:43 +0800

    update

--- app/js/feedBackApp.js
+++ app/js/feedBackApp.js
@@ -8,6 +8,7 @@ app.controller('feedBackCtrl', ['$scope','$http','$q',function($scope,$http,$q) 
             url:url
         }).then(function(data){
             $scope.data=data.data;
+            console.log(data.data);
 
             //Negotiation Results
             $scope.supplierNegotiationResults = {
@@ -57,7 +58,7 @@ app.controller('feedBackCtrl', ['$scope','$http','$q',function($scope,$http,$q) 
             $scope.retailerNegotiationResults = {
                 options: {
                     title:{
-                        text:'Breakdown of Volume Discounts given by Suppliers',
+                        text:'Breakdown of Volume Discounts received by Retailers',
                     },
                     chart: {
                         type: 'pie',
@@ -97,64 +98,184 @@ app.controller('feedBackCtrl', ['$scope','$http','$q',function($scope,$http,$q) 
                 loading: false
             }
 
+            $scope.supplierPerformanceBonuses = {
+                options: {
+                    title:{
+                        text:'Breakdown of Performance Bonuses given by Suppliers',
+                    },
+                    chart: {
+                        type: 'pie',
+                        backgroundColor: 'transparent',
+                    },
+                    legend: {
+                        layout: 'vertical',
+                        align: 'right',
+                        verticalAlign: 'top',
+                        x:0,
+                        y:20
+                    },
+                    tooltip: {
+                        formatter: function() {
+                            var s = '<p><b>'+this.key+'</b></p>'+'<p>'+this.point.y.toFixed(2)+'($mln)</p>'+'<p>'+this.point.percentage.toFixed(2)+'%</p>';
+                            return s;
+                        },
+                        shared: false,
+                        useHTML: true
+                    },
+                    plotOptions: {
+                        pie: {
+                            dataLabels: {
+                                enabled: false
+                            },
+                            showInLegend: true
+                        }
+                    }
+                },
+                series: [{
+                    type: 'pie',
+                    data: [
+                        {'name':'supplier-1',y:data.data.f_PerformanceBonusesValue[0].fcni_SuppliersCost[0].value,'color':'#3257A7'},
+                        {'name':'supplier-2',y:data.data.f_PerformanceBonusesValue[0].fcni_SuppliersCost[1].value,'color':'#B11E22'},
+                        {'name':'supplier-3',y:data.data.f_PerformanceBonusesValue[0].fcni_SuppliersCost[2].value,'color':'#F6B920'}
+                    ]
+                }],
+                loading: false
+            }
+
+            $scope.retailerPerformanceBonuses = {
+                options: {
+                    title:{
+                        text:'Breakdown of Performance Bonuses received by Retailers',
+                    },
+                    chart: {
+                        type: 'pie',
+                        backgroundColor: 'transparent',
+                    },
+                    legend: {
+                        layout: 'vertical',
+                        align: 'right',
+                        verticalAlign: 'top',
+                        x:0,
+                        y:20
+                    },
+                    tooltip: {
+                        formatter: function() {
+                            var s = '<p><b>'+this.key+'</b></p>'+'<p>'+this.point.y.toFixed(2)+'($mln)</p>'+'<p>'+this.point.percentage.toFixed(2)+'%</p>';
+                            return s;
+                        },
+                        shared: false,
+                        useHTML: true
+                    },
+                    plotOptions: {
+                        pie: {
+                            dataLabels: {
+                                enabled: false
+                            },
+                            showInLegend: true
+                        }
+                    }
+                },
+                series: [{
+                    type: 'pie',
+                    data: [
+                        {'name':'retailer-1',y:data.data.f_PerformanceBonusesValue[0].fcni_RetailersBenefits[0].value,'color':'#8B288B'},
+                        {'name':'retailer-2',y:data.data.f_PerformanceBonusesValue[0].fcni_RetailersBenefits[1].value,'color':'#F05422'}
+                    ]
+                }],
+                loading: false
+            }
+
+            $scope.supplierOtherCompensation = {
+                options: {
+                    title:{
+                        text:'Breakdown of Other Compensation given by Suppliers',
+                    },
+                    chart: {
+                        type: 'pie',
+                        backgroundColor: 'transparent',
+                    },
+                    legend: {
+                        layout: 'vertical',
+                        align: 'right',
+                        verticalAlign: 'top',
+                        x:0,
+                        y:20
+                    },
+                    tooltip: {
+                        formatter: function() {
+                            var s = '<p><b>'+this.key+'</b></p>'+'<p>'+this.point.y.toFixed(2)+'($mln)</p>'+'<p>'+this.point.percentage.toFixed(2)+'%</p>';
+                            return s;
+                        },
+                        shared: false,
+                        useHTML: true
+                    },
+                    plotOptions: {
+                        pie: {
+                            dataLabels: {
+                                enabled: false
+                            },
+                            showInLegend: true
+                        }
+                    }
+                },
+                series: [{
+                    type: 'pie',
+                    data: [
+                        {'name':'supplier-1',y:data.data.f_OtherCompensationsValue[0].fcni_SuppliersCost[0].value,'color':'#3257A7'},
+                        {'name':'supplier-2',y:data.data.f_OtherCompensationsValue[0].fcni_SuppliersCost[1].value,'color':'#B11E22'},
+                        {'name':'supplier-3',y:data.data.f_OtherCompensationsValue[0].fcni_SuppliersCost[2].value,'color':'#F6B920'}
+                    ]
+                }],
+                loading: false
+            }
+
+            $scope.retailerOtherCompensation = {
+                options: {
+                    title:{
+                        text:'Breakdown of Other Compensation received by Retailers',
+                    },
+                    chart: {
+                        type: 'pie',
+                        backgroundColor: 'transparent',
+                    },
+                    legend: {
+                        layout: 'vertical',
+                        align: 'right',
+                        verticalAlign: 'top',
+                        x:0,
+                        y:20
+                    },
+                    tooltip: {
+                        formatter: function() {
+                            var s = '<p><b>'+this.key+'</b></p>'+'<p>'+this.point.y.toFixed(2)+'($mln)</p>'+'<p>'+this.point.percentage.toFixed(2)+'%</p>';
+                            return s;
+                        },
+                        shared: false,
+                        useHTML: true
+                    },
+                    plotOptions: {
+                        pie: {
+                            dataLabels: {
+                                enabled: false
+                            },
+                            showInLegend: true
+                        }
+                    }
+                },
+                series: [{
+                    type: 'pie',
+                    data: [
+                        {'name':'retailer-1',y:data.data.f_OtherCompensationsValue[0].fcni_RetailersBenefits[0].value,'color':'#8B288B'},
+                        {'name':'retailer-2',y:data.data.f_OtherCompensationsValue[0].fcni_RetailersBenefits[1].value,'color':'#F05422'}
+                    ]
+                }],
+                loading: false
+            }
 
         });
     }
 
-    var loadNegotiationResults=function(){
-
-        $scope.chartConfig = {
-            options: {
-                title:{
-                    text:'Breakdown of Volume Discounts given by Suppliers',
-                },
-                chart: {
-                    type: 'pie',
-                    backgroundColor: 'transparent',
-                },
-                legend: {
-                    layout: 'vertical',
-                    align: 'right',
-                    verticalAlign: 'top',
-                    x:0,
-                    y:20
-                },
-                tooltip: {
-                    formatter: function() {
-                        var s = '<p><b>'+this.key+'</b></p>'+'<p>'+this.point.y.toFixed(2)+'($mln)</p>'+'<p>'+this.point.percentage.toFixed(2)+'%</p>';
-                        return s;
-                    },
-                    shared: false,
-                    useHTML: true
-                },
-                plotOptions: {
-                    pie: {
-                        dataLabels: {
-                            enabled: false
-                        },
-                        showInLegend: true
-                    }
-                }
-            },
-            series: [{
-                type: 'pie',
-                data: [
-                    // {'name':'supplier-1',y:10,'color':'#3257A7'},
-                    // {'name':'supplier-2',y:90,'color':'#B11E22'},
-                    // {'name':'supplier-3',y:100,'color':'#F6B920'}
-                    {'name':'retailer-2',y:90,'color':'#8B288B'},
-                    {'name':'retailer-3',y:100,'color':'#F05422'}
-                ]
-            }],
-
-            loading: false
-        }
-      }
-
-      $scope.initPage=initPage;
-
-      initPage();
-      loadNegotiationResults();
-    }
-]);
+    $scope.initPage=initPage;
+    initPage();
+}]);
 
