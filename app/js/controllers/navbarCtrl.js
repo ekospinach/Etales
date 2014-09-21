8d95f7258bb115f1c3740d42d94d076cdc0772fc
ludwik.applechina@gmail.com <ludwik.applechina@gmail.com>
2014-09-21 16:11:16 +0800

    quick fix bug : Period info is not modified
     Period info is not modified if Admin set a new one directly

--- app/js/controllers/navbarCtrl.js
+++ app/js/controllers/navbarCtrl.js
@@ -66,16 +66,20 @@ define(['app'], function(app) {
 		});
 
 		$scope.$on('SeminarPeriodChanged', function(event, data) {  
-			notify('Period has been changed to ' + data.period + ' / ' + data.span);
-			$scope.currentPeriod = data.currentPeriod;
-			$scope.span = data.simulationSpan;
-			$scope.seminar = data.seminarCode;
+			if(data.seminarCode == SeminarInfo.getSelectedSeminar().seminarCode ){
+				notify('Period has been changed to ' + data.currentPeriod + ' / ' + data.simulationSpan);
+				$scope.currentPeriod = data.currentPeriod;
+				$scope.span = data.simulationSpan;
+				$scope.seminar = data.seminarCode;				
+			}
 		})	
 
-		$scope.$on('SeminarPeriodChangedFromRoute',function(event, data){
-			$scope.currentPeriod = data.currentPeriod;
-			$scope.span = data.simulationSpan;
-			$scope.seminar = data.seminarCode;
+		$scope.$on('SeminarPeriodChangedFromRoute',function(event, data){	
+			if(data.seminarCode == SeminarInfo.getSelectedSeminar().seminarCode ){
+				$scope.currentPeriod = data.currentPeriod;
+				$scope.span = data.simulationSpan;
+				$scope.seminar = data.seminarCode;
+			}				
 		})	
 
 		//Register socketIO listeners in NavCtrl which will only be activated once in application
