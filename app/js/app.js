c9963c46f71e735a4d98c3f23366f97707fdc595
ludwik.applechina@gmail.com <ludwik.applechina@gmail.com>
2014-10-23 17:35:14 +0800

    update language

--- app/js/app.js
+++ app/js/app.js
@@ -181,7 +181,7 @@
  		])
  		.config(function(LabelProvider) {
  			//config default language
- 			LabelProvider.initialiseLanguage('ENG');
+ 			LabelProvider.initialiseLanguage('CHN');
  		}).config(function(cfpLoadingBarProvider) {
  			cfpLoadingBarProvider.includeSpinner = true;
  		});
