057f6f9f9e12042690da3822202e9b14f73e3d04
raven <monsoul@msn.com>
2014-10-25 08:28:48 +0800

    update retailerdecision

--- app/js/services.js
+++ app/js/services.js
@@ -800,7 +800,7 @@ define(['angular',
 					var socket = io.connect();
 					socket.on('socketIO:retailerBaseChanged', function(data){	
 						//if changed base is modified by current retailer & seminar, reload decision base and broadcast message...
-						if( (data.retailerID ==  PlayerInfo.getPlayer()) && (data.seminar == SeminarInfo.getSelectedSeminar().seminarCode) ){
+						if( (data.retailerID ==  PlayerInfo.getPlayer()) && (data.seminar == SeminarInfo.getSelectedSeminar().seminarCode) &&(data.period == PeriodInfo.getDecisionPeriod())){
 							requestPara.retailerID = parseInt(PlayerInfo.getPlayer());
 							requestPara.period = PeriodInfo.getDecisionPeriod();
 							requestPara.seminar = SeminarInfo.getSelectedSeminar().seminarCode;							
@@ -815,7 +815,7 @@ define(['angular',
 
 					socket.on('socketIO:retailerReportPurchaseDecisionChanged', function(data) {
 						//if changed base is modified by current supplier & seminar, reload Report Purchase decision base and broadcast message...
-						if ((data.retailerID == PlayerInfo.getPlayer()) && (data.seminar == SeminarInfo.getSelectedSeminar().seminarCode)) {
+						if ((data.retailerID == PlayerInfo.getPlayer()) && (data.seminar == SeminarInfo.getSelectedSeminar().seminarCode)&&(data.period == PeriodInfo.getDecisionPeriod())) {
 							var url = '/seminarInfo/' + SeminarInfo.getSelectedSeminar().seminarCode;
 							$http({
 								method: 'GET',
