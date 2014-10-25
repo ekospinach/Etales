90adb64a801bc3eac2b0c82ec97e47c4c03525f0
ludwik.applechina@gmail.com <ludwik.applechina@gmail.com>
2014-10-25 10:37:20 +0800

    update kernel

--- scripts/decisionsExport.sh
+++ scripts/decisionsExport.sh
@@ -9,15 +9,15 @@ mongoexport -h 115.29.35.224 -d Etales -c contractvariantdetails -q "{'seminar':
 mongoexport -h 115.29.35.224 -d Etales -c contracts -q "{'seminar':'ETALES01'}" -o ./contracts.data
 
 
-db.prodecisions.remove({seminar:'PRECOMMERCIAL'})
-db.retailerdecisions.remove({seminar:'PRECOMMERCIAL'})
-db.contractvariantdetails.remove({seminar:'PRECOMMERCIAL'})
-db.contracts.remove({seminar:'PRECOMMERCIAL'})
+db.prodecisions.remove({seminar:'ETALES01'})
+db.retailerdecisions.remove({seminar:'ETALES01'})
+db.contractvariantdetails.remove({seminar:'ETALES01'})
+db.contracts.remove({seminar:'ETALES01'})
 
-mongoimport -d Etales -c prodecisions prodecisions.data
-mongoimport -d Etales -c retailerdecisions retailerdecisions.data
-mongoimport -d Etales -c contractvariantdetails contractvariantdetails.data
-mongoimport -d Etales -c contracts contracts.data
+mongoimport -d Etales -c prodecisions ./prodecisions.data
+mongoimport -d Etales -c retailerdecisions ./retailerdecisions.data
+mongoimport -d Etales -c contractvariantdetails ./contractvariantdetails.data
+mongoimport -d Etales -c contracts ./contracts.data
 
 #备份数据库/恢复数据库
 mongodump -h dbhost -d dbname -o dbdirectory
