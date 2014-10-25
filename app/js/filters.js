0f1c32078cdd14e81d01819e0738b60a1aefd398
raven <monsoul@msn.com>
2014-10-25 19:51:12 +0800

    fix mr

--- app/js/filters.js
+++ app/js/filters.js
@@ -13,15 +13,23 @@ define(['angular', 'services'], function (angular, services) {
 		        return Math.round(item*10)/10;
 		    }
 		})
-		.filter('isNull',function(){
+		.filter('NulltoMinOrNumber1',function(){
 			return function(item){
 				if(item==0||item==-100){
 					return '-';
 				}else{
 					return Math.round(item*10)/10;
 				}
-				return item;
 			}
 		});
+		.filter('NulltoMinOrNumber2',function(num){
+			return function(item){
+				if(item==0||item==-100){
+					return '-';
+				}else{
+					return Math.round(item*100)/100;
+				}
+			}
+		})
 
 });
