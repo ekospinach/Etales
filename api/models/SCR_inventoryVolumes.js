f55dce8b0abe15a1fc4e3b669dac65eeebffe7c8
monsoul <monsoul@msn.com>
2014-12-02 10:59:55 +0800

    eidt rcr and score model
    删除假数据
    添加err 返回

--- api/models/SCR_inventoryVolumes.js
+++ api/models/SCR_inventoryVolumes.js
@@ -106,92 +106,22 @@ exports.addReports = function(options){
     return deferred.promise;
 }
 
-
-
-exports.addSCR_inventoryVolumes=function(req,res,next){
-    var newSCR_inventoryVolumes=SCR_inventoryVolumes({
-        period : 0,
-        seminar : 'MAY',
-        producerID  : 1, //TAllProducer : 1~4 (ProsMaxPlus)
-        scrviv_Initial          : [{
-            variantName:'_A',
-            parentBrandName:'ELAN1',
-            parentCategoryID:1,
-            value:[10,20,30]
-        },{
-            variantName:'_B',
-            parentBrandName:'HLAN1',
-            parentCategoryID:2,
-            value:[40,50,60]
-        }],
-        scrviv_Production       : [{
-            variantName:'_A',
-            parentBrandName:'ELAN1',
-            parentCategoryID:1,
-            value:[10,20,30]
-        },{
-            variantName:'_B',
-            parentBrandName:'HLAN1',
-            parentCategoryID:2,
-            value:[40,50,60]
-        }],
-        scrviv_Sales            : [{
-            variantName:'_A',
-            parentBrandName:'ELAN1',
-            parentCategoryID:1,
-            value:[10,20,30]
-        },{
-            variantName:'_B',
-            parentBrandName:'HLAN1',
-            parentCategoryID:2,
-            value:[40,50,60]
-        }],
-        scrviv_Discontinued     : [{
-            variantName:'_A',
-            parentBrandName:'ELAN1',
-            parentCategoryID:1,
-            value:[10,20,30]
-        },{
-            variantName:'_B',
-            parentBrandName:'HLAN1',
-            parentCategoryID:2,
-            value:[40,50,60]
-        }],
-        scrviv_Closing          : [{
-            variantName:'_A',
-            parentBrandName:'ELAN1',
-            parentCategoryID:1,
-            value:[10,20,30]
-        },{
-            variantName:'_B',
-            parentBrandName:'HLAN1',
-            parentCategoryID:2,
-            value:[40,50,60]
-        }]
-    });
-    newSCR_inventoryVolumes.save(function(err) {
-        if(!err){
-            res.send(200,newSCR_inventoryVolumes);
-            console.log("created new GeneralReport:"+newSCR_inventoryVolumes);
-        } else {
-            res.send(400,"failed.");
-        }
-    });  
-}
-
-exports.getSCR_inventoryVolumes=function(req,res,next){
-    var data={
-        'seminar':req.params.seminar,
-        'period':req.params.period,
-        'producerID':req.params.producerID
+exports.getSCR_inventoryVolumes = function(req, res, next) {
+    var data = {
+        'seminar': req.params.seminar,
+        'period': req.params.period,
+        'producerID': req.params.producerID
     };
-    SCR_inventoryVolumes.find(data,function(err,docs){
-        if(docs){
-            res.send(200,docs);
-        }else{
-            res.send(404,'failed');
+    SCR_inventoryVolumes.find(data, function(err, docs) {
+        if (err) {
+            return next(new Error(err));
         }
-    })  
+        if (docs) {
+            res.send(200, docs);
+        } else {
+            res.send(404, 'failed');
+        }
+    })
 }
 
 exports.getSCR_ClosingInternetInventoryVolume = function(req, res, next) {
@@ -202,21 +132,23 @@ exports.getSCR_ClosingInternetInventoryVolume = function(req, res, next) {
     };
     var responseResult;
     SCR_inventoryVolumes.findOne(data, function(err, doc) {
+        if (err) {
+            return next(new Error(err));
+        }
         if (doc) {
-            console.log(doc.scrviv_Closing);
             for (i = 0; i < doc.scrviv_Closing.length; i++) {
                 if (doc.scrviv_Closing[i].parentBrandName == req.params.brandName && doc.scrviv_Closing[i].variantName == req.params.varName) {
                     responseResult = doc.scrviv_Closing[i].value[1];
                 }
             }
-        } 
+        }
 
-        if(responseResult){
+        if (responseResult) {
             res.send(200, {
                 'result': responseResult
             });
         } else {
-           res.send(404, 'failed'); 
+            res.send(404, 'failed');
         }
     })
 }
