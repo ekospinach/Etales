162338e330af314deb87cc70e1bd1626061ab33d
monsoul <monsoul@msn.com>
2015-05-27 16:47:19 +0800

    fix 4 & 6

--- app/js/feedBackAppControllers/retailerProfitCtrl.js
+++ app/js/feedBackAppControllers/retailerProfitCtrl.js
@@ -14,7 +14,91 @@ var retailerProfitCtrl = function($scope, $http, PlayerColor, Label) {
 
     var organiseGrossArray = function(data, periods) {
 
-    }
+        var result = {
+            data: [{
+                name: Label.getContent('Retailer') + ' 1',
+                data: [1,1,1,1,null,1,1,1,1,null,1,1,1,1,null,1,1],
+                color: PlayerColor.r1,
+                xAxis: 0
+            }, {
+                name: Label.getContent('Retailer') + ' 2',
+                data: [1,1,1,1,null,1,1,1,1,null,1,1,1,1,null,1,1],
+                color: PlayerColor.r2,
+                xAxis: 0
+            }, {
+                name: Label.getContent('Supplier') + ' 1',
+                data: [1,1,1,1,null,1,1,1,1,null,1,1,1,1,null,1,1],
+                color: PlayerColor.s1,
+                xAxis: 0
+            }, {
+                name: Label.getContent('Supplier') + ' 2',
+                data: [1,1,1,1,null,1,1,1,1,null,1,1,1,1,null,1,1],
+                color: PlayerColor.s2,
+                xAxis: 0
+            }, {
+                name: Label.getContent('Supplier') + ' 3',
+                data: [1,1,1,1,null,1,1,1,1,null,1,1,1,1,null,1,1],
+                color: PlayerColor.s3,
+                xAxis: 0
+            }, {
+                name: ' ',
+                data: [null, null, null, null, null, null, null, null, null, null],
+                color: 'transparent',
+                xAxis: 1, //第2个X轴
+            }, {
+                name: ' ',
+                data: [null,null,null,null],
+                color: 'transparent',
+                xAxis: 2, //第3个X轴
+            }],
+            categories: [],
+            subCategories:[],
+            thirdCategories:[]
+        }
+
+        // for (var i = 0; i < 7 * periods.length + 4; i++) {
+        //     result.data[0].data[i] = 1;
+        //     result.data[1].data[i] = 1;
+        //     result.data[2].data[i] = 1;
+        //     result.data[3].data[i] = 1;
+        //     result.data[4].data[i] = 1;
+        // }
+        for(var i=0;i<2;i++){
+            result.categories.push(periods[i]);
+        }
+        for(var i=0;i<2;i++){
+            result.categories.push(periods[i]);
+        }
+        result.categories.push(' ');
+        for(var i=0;i<2;i++){
+            result.categories.push(periods[i]);
+        }
+        for(var i=0;i<2;i++){
+            result.categories.push(periods[i]);
+        }
+        result.categories.push(' ');
+        for(var i=0;i<2;i++){
+            result.categories.push(periods[i]);
+        }
+        for(var i=0;i<2;i++){
+            result.categories.push(periods[i]);
+        }
+        result.categories.push(' ');
+        for(var i=0;i<2;i++){
+            result.categories.push(periods[i]);
+        }
+
+
+        result.subCategories=[Label.getContent('Retailer')+' 1',Label.getContent('Retailer')+' 2',' ',Label.getContent('Retailer')+' 1',Label.getContent('Retailer')+' 2',' ',Label.getContent('Retailer')+' 1',Label.getContent('Retailer')+' 2',' ',' ']
+        result.thirdCategories=[Label.getContent('Urban Market'),Label.getContent('Rural Market'),Label.getContent('Total Market'),Label.getContent('Online Market')]
+
+        return result;
+
+
+        // periods.forEach(function(singlePeriod){
+
+        // })
+    }   
 
     var organiseMarginArray = function(data, periods) {
 
@@ -80,13 +164,18 @@ var retailerProfitCtrl = function($scope, $http, PlayerColor, Label) {
     var initPage = function() {
         var Request = GetRequest();
         var periods = [];
+        var grossPeriods = [];
+
         for (var i = -3; i <= Request['period']; i++) {
             periods.push(i);
+        }
+        for (var i = Request['period'] - 1; i <= Request['period']; i++) {
+            grossPeriods.push(i);
         }
         var result = {
             'profit': {
                 data: {},
-                categories:{}
+                categories: {}
             },
             'gross': {
                 data: {}
@@ -98,10 +187,97 @@ var retailerProfitCtrl = function($scope, $http, PlayerColor, Label) {
                 data: {}
             }
         }
-        result.profit = organiseGrossArray($scope.xf_RetailerGrossProfitPerBrandOwner, periods);
+        result.profit = organiseGrossArray($scope.xf_RetailerGrossProfitPerBrandOwner, grossPeriods);
         result.gross = organiseMarginArray($scope.feedback.xf_StoreGrossProfitMargin, periods);
         result.operating = organiseMarginArray($scope.feedback.xf_StoreOperatingProfitMargin, periods);
         result.net = organiseMarginArray($scope.feedback.xf_StoreNetProfitMargin, periods);
+
+        $scope.profitGross = {
+            options: {
+                xAxis: [{
+                    categories: result.profit.categories,
+                    tickWidth: 0,
+                    gridLineWidth: 0
+                }, {
+                    categories: result.profit.subCategories,
+                    labels: {
+                        style: {
+                            fontSize: '18px',
+                            'color': '#f26c4f',
+                            'align': 'right'
+                        },
+                    },
+                    lineWidth: 0,
+                    tickWidth: 0
+                }, {
+                    categories: result.profit.thirdCategories,
+                    labels: {
+                        style: {
+                            fontSize: '18px',
+                            'color': '#f26c4f',
+                        },
+                        y: -30
+                    },
+                    tickWidth: 0,
+                    opposite: true //对立面 表示显示在上方
+                }],
+                yAxis: {
+                    title: {
+                        text: '%'
+                    },
+                    max: 100
+                },
+                chart: {
+                    type: 'column',
+                    backgroundColor: 'transparent'
+                },
+                tooltip: {
+                    formatter: function() {
+                        var s = '<p><b>' + this.key + '</b></p>' + '<p>' + this.series.name + ':' + this.point.y.toFixed(2) + ' %</p>';
+                        return s;
+                    },
+                    shared: false,
+                    useHTML: true
+                },
+                plotOptions: {
+                    column: {
+                        stacking: 'normal',
+                        dataLabels: {
+                            enabled: true,
+                            color: 'white',
+                            style: {
+                                textShadow: '0 0 3px black',
+                                fontSize: '18px'
+                            },
+                            formatter: function() {
+                                if (this.y != null) {
+                                    return this.y.toFixed(2) + '%'
+                                } else {
+                                    return "";
+                                }
+
+                            }
+                        }
+                    },
+                    series: {
+                        stacking: 'percent'
+                    },
+
+                },
+                legend: {
+                    borderWidth: 0,
+                    x: 50,
+                }
+            },
+            title: {
+                text: ''
+            },
+            series: result.profit.data,
+            credits: {
+                enabled: false
+            },
+            loading: false
+        }
 
         $scope.grossProfitsMargins = {
             options: {
