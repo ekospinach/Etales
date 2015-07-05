c30308a4ed3c749aab68f59780b2025e9cb5523f
monsoul <monsoul@msn.com>
2015-07-05 17:04:39 +0800

    fix

--- app/js/feedBackAppControllers/supplierExtendCtrl.js
+++ app/js/feedBackAppControllers/supplierExtendCtrl.js
@@ -21,17 +21,17 @@ app.controller('supplierExtendCtrl', ['$scope', '$http', '$q', 'Label', 'StaticV
             var variantPerceptionEvolutionUrl = '/getMR-variantPerceptionEvolution/' + Request['seminar'] + '/' + Request['period'];
             var netMarketPricesUrl = '/getMR-netMarketPrices/' + Request['seminar'] + '/' + Request['period'];
             var pricePromotionsUrl = '/getMR-pricePromotions/' + Request['seminar'] + '/' + Request['period'];
-
+            var awarenessUrl = '/getMR-awarenessEvolution/' + Request['seminar'] + '/' + Request['period'];
 
             $q.all([
-                $http.get(extendedFeedbackUrl), $http.get(normalfeedbackUrl), $http.get(variantPerceptionEvolutionUrl), $http.get(netMarketPricesUrl), $http.get(pricePromotionsUrl)
+                $http.get(extendedFeedbackUrl), $http.get(normalfeedbackUrl), $http.get(variantPerceptionEvolutionUrl), $http.get(netMarketPricesUrl), $http.get(pricePromotionsUrl), $http.get(awarenessUrl)
             ]).then(function(data) {
                 $scope.feedback = data[0].data;
                 $scope.normalfeedback = data[1].data;
                 $scope.variantPerception = data[2].data[0];
                 $scope.netMarketPrices = data[3].data[0];
                 $scope.pricePromotions = data[4].data[0];
-
+                $scope.awareness = data[5].data[0];
                 var language = 'ENG';
                 if (Request['language'] != 'English')
                     language = 'CHN';
